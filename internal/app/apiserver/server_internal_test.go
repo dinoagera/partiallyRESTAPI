c2716d41d67987ab8ae6ede95a17eb4dcaa222a0
Almaz Husnutdinof <husnutdinoff.almaz2017@yandex.ru>
2025-03-02 15:21:11 +0300

    add register user

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -1,6 +1,8 @@
 package apiserver
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"restapi/internal/app/store/teststore"
@@ -9,10 +11,40 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestServer_HandleUsersCre(t *testing.T) {
-	rec := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
+func TestServer_HandleUsersCreate(t *testing.T) {
 	s := newServer(teststore.New())
-	s.ServeHTTP(rec, req)
-	assert.Equal(t, rec.Code, http.StatusOK)
+	testCases := []struct {
+		name         string
+		payload      interface{}
+		expectedCode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]string{
+				"email":    "user@example.org",
+				"password": "password",
+			},
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "invalid payload",
+			payload:      "invalid",
+			expectedCode: http.StatusBadRequest,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			b := &bytes.Buffer{}
+			json.NewEncoder(b).Encode(tc.payload)
+			req, _ := http.NewRequest(http.MethodPost, "/users", b)
+			s.ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedCode, rec.Code)
+		})
+	}
+	// rec := httptest.NewRecorder()
+	// req, _ := http.NewRequest(http.MethodPost, "/users", nil)
+	// s := newServer(teststore.New())
+	// s.ServeHTTP(rec, req)
+	// assert.Equal(t, rec.Code, http.StatusOK)
 }
--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -7,10 +7,10 @@ import (
 )
 
 type User struct {
-	ID                int
-	Email             string
-	Password          string
-	EncryptedPassword string
+	ID                int    `json:"id"`
+	Email             string `json:"email"`
+	Password          string `json:"password,omitempty"`
+	EncryptedPassword string `json:"-"`
 }
 
 func (u *User) BeforeCreate() error {
